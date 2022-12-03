57372a1bfc718c169905b6f108e2aeb2de497872
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-12-03 15:02:31 -0600

    registration

--- src/main/java/com/in28minutes/springboot/MyFirstWebApplication/controllers/UserController.java
+++ src/main/java/com/in28minutes/springboot/MyFirstWebApplication/controllers/UserController.java
@@ -26,7 +26,7 @@ public class UserController {
     @GetMapping("/register")
     public String showRegistrationForm(Model model){
         model.addAttribute("user", new User());
-        return "redirect:/registration";
+        return "/registration";
     }
 
     @PostMapping("/register")
